b6179c296bdd7366d6de6a1c11ced10a3be842d1
Sanchit Khurana <sanchit007khurana@gmail.com>
2019-04-04 10:54:57 +0530

    My First Commit

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={			
+	'ApiUrl':"http://hedgebulls.in/mealz24/driver/api",		
+	'DialogDefaultTitle':"Mealz24",
+	'mapboxToken' : 'mymapboxToken',
+	'APIHasKey':"qazwsxedcrfv1234tbb",
+	'debug': false
+};
